eb232c2641dddc6d82029c55f5979dc12e057b45
tracer-ics2021 <tracer@njuics.org>
2021-09-14 02:52:17 +0800

    >  compile 201220073    Static hostname: cao-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 08c6cf5370d34aa083d22a1fe3d78aaa            Boot ID: 5c49199696b94975918147d393940a0b     Virtualization: oracle   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  02:52:18 up  1:31,  1 user,  load average: 0.09, 0.10, 0.09

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -5,7 +5,17 @@ void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
 
+#include <isa.h>
+void check(){
+	FILE *fp;
+	fp=fopen("input","r");
+	extern word_t expr(char *e,bool *success);
+	word_t x;char *st="";bool success=0;
+	while(fscanf(fp,"%u%s",&x,st)!=EOF) assert(x==expr(st,&success));
+}
+
 int main(int argc, char *argv[]) {
+	check();
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
   am_init_monitor();
