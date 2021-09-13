18407d0820f26b340d6a4bbb77e5a0030ea598ed
tracer-ics2021 <tracer@njuics.org>
2021-09-13 13:13:17 +0800

    >  compile 201220073    Static hostname: cao-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 08c6cf5370d34aa083d22a1fe3d78aaa            Boot ID: ae70e1cab1b84cc3a1248bbba5334883     Virtualization: oracle   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  13:13:17 up  1:34,  1 user,  load average: 0.16, 0.11, 0.09

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -9,7 +9,7 @@ enum {
   TK_NOTYPE = 256, TK_EQ,
 
   /* TODO: Add more token types */
-
+  TK_NUM,
 };
 
 static struct rule {
@@ -24,6 +24,13 @@ static struct rule {
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
   {"==", TK_EQ},        // equal
+  
+  {"\\-",'-'},			//minus
+  {"\\*",'*'},
+  {"\\/",'/'},
+  {"[0-9]+",TK_NUM},
+  {"\\(",'('},
+  {"\\)",')'}
 };
 
 #define NR_REGEX ARRLEN(rules)
@@ -80,7 +87,12 @@ static bool make_token(char *e) {
          */
 
         switch (rules[i].token_type) {
-          default: TODO();
+		  case TK_NOTYPE:break;
+          default:{
+			tokens[nr_token].type=rules[i].token_type;
+			strncpy(tokens[nr_token++].str,substr_start,substr_len);
+			break;
+		  }
         }
 
         break;
