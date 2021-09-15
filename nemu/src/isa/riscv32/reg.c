2468d66fe61b46fdd042a94bce69d4743c38e29d
tracer-ics2021 <tracer@njuics.org>
2021-09-15 22:29:54 +0800

    >  compile 201220073    Static hostname: cao-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 08c6cf5370d34aa083d22a1fe3d78aaa            Boot ID: e994d1471bf74015a7e1e05d2decf3dd     Virtualization: oracle   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  22:29:54 up  4:20,  1 user,  load average: 0.13, 0.12, 0.09

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -17,8 +17,8 @@ void isa_reg_display() {
 word_t isa_reg_str2val(const char *s, bool *success) {
 	printf("%s\n",s);
 	for(int i=0;i<32;++i){
-		printf("%s ",regs[i]);
-		if(strcmp(s,regs[i])==0){
+//		printf("%s ",regs[i]);
+		if(strcmp(s+1,regs[i])==0){
 			*success=true;
 			return gpr(i);
 		}
