cc4daf220c308600f8023babe32c18038d9aaed6
tracer-ics2021 <tracer@njuics.org>
2021-09-16 01:30:51 +0800

    >  compile 201220073    Static hostname: cao-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 08c6cf5370d34aa083d22a1fe3d78aaa            Boot ID: e994d1471bf74015a7e1e05d2decf3dd     Virtualization: oracle   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  01:30:51 up  7:21,  1 user,  load average: 0.05, 0.10, 0.05

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -7,7 +7,9 @@ typedef struct watchpoint {
   struct watchpoint *next;
 
   /* TODO: Add more members if necessary */
-
+  word_t data;
+  char* exp;
+  bool success;
 } WP;
 
 static WP wp_pool[NR_WP] = {};
@@ -15,7 +17,7 @@ static WP *head = NULL, *free_ = NULL;
 
 void init_wp_pool() {
   int i;
-  for (i = 0; i < NR_WP; i ++) {
+   for (i = 0; i < NR_WP; i ++) {
     wp_pool[i].NO = i;
     wp_pool[i].next = &wp_pool[i + 1];
   }
@@ -26,4 +28,24 @@ void init_wp_pool() {
 }
 
 /* TODO: Implement the functionality of watchpoint */
+WP* new_wp(char *args){
+	if(free_==NULL) assert(0);
+	WP *ans=free_;
+	free_=free_->next;
+	ans->next=head;
+	head=ans;
+	
+	strcpy(ans->exp,args);
+	ans->success=1;
+	ans->data=expr(args,&ans->success);
 
+	Log("Watch point %d has been built.\n",ans->NO);
+	return ans;
+}
+
+void free_wp(WP *wp){
+	wp->next=free_;
+	free_=wp;
+	Log("Watch point %d has been freed.\n",wp->NO);
+	return;
+}
