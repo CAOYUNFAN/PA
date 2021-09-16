d7ebeaec8762aa5d53a55f3a8ba6571190c7384e
tracer-ics2021 <tracer@njuics.org>
2021-09-16 18:42:05 +0800

    >  compile 201220073    Static hostname: cao-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 08c6cf5370d34aa083d22a1fe3d78aaa            Boot ID: aad57de7fb6042f8a3d56c76188bf054     Virtualization: oracle   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  18:42:05 up  6:58,  1 user,  load average: 0.01, 0.13, 0.15

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -9,18 +9,18 @@ int is_exit_status_bad();
 void check(){
 	FILE *fp;
 	fp=fopen("/home/cao/ics2021/nemu/tools/gen-expr/input","r");
-//	assert(fp!=NULL);
-	if(fp!=NULL) printf("OPEN SUCCEED!\n");
+	assert(fp!=NULL);
+//	if(fp!=NULL) printf("OPEN SUCCEED!\n");
 	extern word_t expr(char *e,bool *success);
 	word_t x;char st[10000],ch[10000];bool success=0;
- 	while(fgets(ch,1000,fp)!=NULL){
+  	while(fgets(ch,1000,fp)!=NULL){
 //		printf("%s\n",ch);
 		int i;
 		x=0;
 		for(i=0;ch[i]!=' ';++i) x=(x*10u)+ch[i]-'0';
 //		printf("%u ",x);
 		strcpy(st,ch+i+1);
-		printf("%s\n",st);
+		printf("%s",st);
 		word_t temp=expr(st,&success);
 		printf("%u\n",temp);
 		if(!success||x!=temp) assert(0);
