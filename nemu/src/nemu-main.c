ad5368818296e6a023603676d0fe798d044616f7
tracer-ics2021 <tracer@njuics.org>
2021-09-16 18:47:24 +0800

    >  compile 201220073    Static hostname: cao-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 08c6cf5370d34aa083d22a1fe3d78aaa            Boot ID: aad57de7fb6042f8a3d56c76188bf054     Virtualization: oracle   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  18:47:24 up  7:03,  1 user,  load average: 0.11, 0.13, 0.14

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -18,7 +18,7 @@ void check(){
 		int i;
 		x=0;
 		for(i=0;ch[i]!=' ';++i) x=(x*10u)+ch[i]-'0';
-//		printf("%u ",x);
+		printf("%u ",x);
 		int j=0;
 		for(i++;i<strlen(ch)-1;i++,j++) st[j]=ch[i];
 		st[j+1]='\0';
