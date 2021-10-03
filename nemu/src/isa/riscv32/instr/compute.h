5bb373af168d0dfdab3e4a5bd10b17ce1e72a233
tracer-ics2021 <tracer@njuics.org>
2021-10-04 03:41:07 +0800

    >  compile 201220073    Static hostname: cao-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c            Boot ID: 4436f29a0f3340b89543af8330df7674     Virtualization: oracle   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  03:41:07 up  9:56,  1 user,  load average: 0.26, 0.14, 0.10

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -2,7 +2,7 @@ def_EHelper(lui) {
   rtl_li(s, ddest, id_src1->imm);
 }
 def_EHelper(auipc){
-	rtl_addi(s,id_dest->preg,&s->snpc,id_src1->imm);
+	rtl_addi(s,id_dest->preg,&s->pc,id_src1->imm);
 }
 def_EHelper(addi){
 	rtl_addi(s,id_dest->preg,id_src1->preg,id_src2->imm);
