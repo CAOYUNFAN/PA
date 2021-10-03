8825a229b7c0c35bd7c16219cb4153605bd45f7b
tracer-ics2021 <tracer@njuics.org>
2021-10-04 02:03:41 +0800

    >  compile 201220073    Static hostname: cao-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c            Boot ID: 4436f29a0f3340b89543af8330df7674     Virtualization: oracle   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  02:03:41 up  8:24,  1 user,  load average: 0.24, 0.15, 0.11

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -2,8 +2,8 @@
 #include "../local-include/rtl.h"
 
 #define INSTR_NULLARY(f) f(inv) f(nemu_trap)
-#define INSTR_UNARY(f)
-#define INSTR_BINARY(f) f(lui)
-#define INSTR_TERNARY(f) f(lw) f(sw)
+#define INSTR_UNARY(f) 
+#define INSTR_BINARY(f) f(lui) f(auipc) f(jal)
+#define INSTR_TERNARY(f) f(lw) f(sw) f(addi)
 
 def_all_EXEC_ID();
--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -1,3 +1,16 @@
 def_EHelper(lui) {
   rtl_li(s, ddest, id_src1->imm);
 }
+def_EHelper(auipc){
+	rtl_addi(s,id_dest->preg,&s->snpc,id_src1->imm);
+}
+def_EHelper(addi){
+	rtl_addi(s,id_dest->preg,id_src1->preg,id_src2->imm);
+}
+def_EHelper(jal){
+	rtl_mv(s,id_dest->preg,&s->snpc);
+	rtl_j(s,id_src1->imm);
+}
+/*def_EHelper(sw){
+	rtl_sm(s,id_dest->preg,id_src1->preg,id_src2->imm,4);
+}*/
