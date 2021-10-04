270a5afe113efcf0e07d7ad4a8595742fc9ffbf3
tracer-ics2021 <tracer@njuics.org>
2021-10-04 19:50:38 +0800

    >  compile 201220073    Static hostname: cao-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c            Boot ID: cb7b08a6047e4f4390c1563848828649     Virtualization: oracle   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  19:50:38 up  3:34,  1 user,  load average: 0.18, 0.13, 0.10

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -33,7 +33,7 @@ static struct rule {
   {"\\)",')'},
   {"0x[0-9a-f]+",TK_HEXNUM},
   {"[0-9]+",TK_NUM},
-  {"\\$[\\$a-z][0-9a-f]",TK_REG},
+  {"\\$[\\$a-z][0-9a-z]",TK_REG},
   {"!=",TK_NEQ},
   {"&&",TK_AND},
   {"\\|\\|",TK_OR},
