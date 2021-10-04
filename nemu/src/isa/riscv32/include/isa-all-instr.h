ea51b92a5df20e7cdecb15935cc2ec1d4b330004
Cao YUnfan <201220073@smail.nju.edu.cn>
2021-10-05 05:08:51 +0800

    fix conflicts

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -1,9 +1,6 @@
 #include <cpu/decode.h>
 #include "../local-include/rtl.h"
 
-#define INSTR_NULLARY(f) f(inv) f(nemu_trap)
-#define INSTR_UNARY(f) 
-#define INSTR_BINARY(f) f(lui) f(auipc) f(jal) f(mulu_lo) f(muls_hi) f(mulsu_hi) f(mulu_hi) f(divs_q) f(divu_q) f(divs_r) f(divu_r)
-#define INSTR_TERNARY(f) f(lw) f(sw) f(addi) f(jalr) f(add) f(slt) f(beq) f(bne) f(sub) f(sltu) f(xor) f(or) f(sb) f(blt) f(bltu) f(bge) f(bgeu) f(srai) f(sra) f(lb) f(lh) f(lbu) f(lhu) f(andi) f(slti) f(xori) f(ori) f(slli) f(srli) f(srl) f(and) f(sltui) f(sll) f(sh) 
+#define INSTR_LIST(f) f(inv) f(nemu_trap) f(lui) f(auipc) f(jal) f(mulu_lo) f(muls_hi) f(mulsu_hi) f(mulu_hi) f(divs_q) f(divu_q) f(divs_r) f(divu_r) f(lw) f(sw) f(addi) f(jalr) f(add) f(slt) f(beq) f(bne) f(sub) f(sltu) f(xor) f(or) f(sb) f(blt) f(bltu) f(bge) f(bgeu) f(srai) f(sra) f(lb) f(lh) f(lbu) f(lhu) f(andi) f(slti) f(xori) f(ori) f(slli) f(srli) f(srl) f(and) f(sltui) f(sll) f(sh) 
 
 def_all_EXEC_ID();
