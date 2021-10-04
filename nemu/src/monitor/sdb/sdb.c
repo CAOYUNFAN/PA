18227e297711c580c9056baa79bdb67f97c9c612
tracer-ics2021 <tracer@njuics.org>
2021-10-04 20:05:56 +0800

    >  compile 201220073    Static hostname: cao-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c            Boot ID: cb7b08a6047e4f4390c1563848828649     Virtualization: oracle   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  20:05:56 up  3:49,  1 user,  load average: 0.02, 0.10, 0.10

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -118,7 +118,7 @@ static int cmd_x(char *args){
 static int cmd_p(char *args){
 	bool success;
 	word_t ans=expr(args,&success);
-	if(success) printf("%u\n",ans);
+	if(success) printf("%u\t%x\n",ans,ans);
 	else printf("Mistakes happen.\n");
 	return 0;
 }
