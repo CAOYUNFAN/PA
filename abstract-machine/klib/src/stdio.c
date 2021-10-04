3dbd7bfb92d0e6e2cb54fc6ee83fbcfc7f9446fa
tracer-ics2021 <tracer@njuics.org>
2021-10-05 04:20:09 +0800

    >  compile 201220073    Static hostname: cao-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c            Boot ID: cb7b08a6047e4f4390c1563848828649     Virtualization: oracle   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  04:20:09 up 10:16,  1 user,  load average: 0.14, 0.13, 0.05

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,29 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+	va_list ap; va_start(ap,fmt);
+	int d;char c;char *st=out;char* s;
+	while(*fmt){
+		if(*fmt=='%'){
+			switch(*(++fmt)){
+				case 's': s=va_arg(ap,char *);
+						for(char *ss=s;*ss;++ss,++st) *st=*ss;
+						break;
+				case 'd':d=va_arg(ap,int);
+						if(d==0){*st++='0';break;}
+						if(d<0){*st++='-';d=-d;}
+						for(;d;d/=10) *st++=d%10+'0';
+						break;
+				case 'c':c=(char)va_arg(ap,int);
+						*st++=c;
+						break;
+				default:panic("Not implemented or error happens");
+			}
+			++fmt;
+		}else *st++=*fmt++;
+	}
+	*st='\0';
+	return st-out;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
