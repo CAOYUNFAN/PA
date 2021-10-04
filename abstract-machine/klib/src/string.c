a6c8ef02d898fc212ab328a119d8b1cd11a93b56
tracer-ics2021 <tracer@njuics.org>
2021-10-05 03:33:11 +0800

    >  compile 201220073    Static hostname: cao-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c            Boot ID: cb7b08a6047e4f4390c1563848828649     Virtualization: oracle   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  03:33:11 up  9:29,  1 user,  load average: 0.14, 0.18, 0.14

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,31 +5,62 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+	size_t i=0;
+	for(;*s++;++i);
+	return i;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+	size_t i;
+	for (i = 0;src[i] != '\0'; i++)
+	   dst[i] = src[i];   
+	dst[i] = '\0';
+	return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  size_t i;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dst[i] = src[i];
+	for ( ; i < n; i++)
+		dst[i] = '\0';
+	return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+	size_t dest_len = strlen(dst);
+	size_t i;
+
+	for (i = 0 ; src[i] != '\0' ; i++)
+		 dst[dest_len + i] = src[i];
+	dst[dest_len + i] = '\0';
+
+	return dst;
+
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  int l1=strlen(s1),l2=strlen(s2);
+	int n=l1<l2?l1:l2;
+	for(int i=0;i<n;++i) if(*(s1+i)!=*(s2+i))
+		return *(s1+i)<*(s2+i)?-1:1;
+	if(l1==l2) return 0;
+	return l1<l2?-1:1;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+	int l1=strlen(s1),l2=strlen(s2);
+	int tot=l1<l2?l1:l2,i;
+	for(i=0;i<tot&&i<n;++i) if(*(s1+i)!=*(s2+i))
+		return *(s1+i)<*(s2+i)?-1:1;
+	if(i==n||l1==l2) return 0;
+	return l1<l2?-1:1;
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+	unsigned char uc=c;
+  for(unsigned char *ss=s;n;--n,++ss) *ss=uc;
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
