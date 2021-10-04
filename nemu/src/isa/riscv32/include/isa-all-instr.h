1b83a22b6b7836e0ee6f006d43626f7503a1f479
tracer-ics2021 <tracer@njuics.org>
2021-10-05 01:03:28 +0800

    >  compile 201220073    Static hostname: cao-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c            Boot ID: cb7b08a6047e4f4390c1563848828649     Virtualization: oracle   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  01:03:28 up  7:04,  1 user,  load average: 0.07, 0.09, 0.10

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -4,6 +4,6 @@
 #define INSTR_NULLARY(f) f(inv) f(nemu_trap)
 #define INSTR_UNARY(f) 
 #define INSTR_BINARY(f) f(lui) f(auipc) f(jal)
-#define INSTR_TERNARY(f) f(lw) f(sw) f(addi) f(jalr) f(add) f(sltiu) f(beq) f(bne) f(sub) f(sltu) f(xor) f(or) f(sb) f(blt) f(bltu) f(bge) f(bgeu) f(srai) f(sra) f(lb) f(lh) f(lbu) f(lhu) f(andi) f(slti) f(xori) f(ori) f(slli) f(srli) 
+#define INSTR_TERNARY(f) f(lw) f(sw) f(addi) f(jalr) f(add) f(slt) f(beq) f(bne) f(sub) f(sltu) f(xor) f(or) f(sb) f(blt) f(bltu) f(bge) f(bgeu) f(srai) f(sra) f(lb) f(lh) f(lbu) f(lhu) f(andi) f(slti) f(xori) f(ori) f(slli) f(srli) f(srl) f(and) f(sltui) 
 
 def_all_EXEC_ID();
--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -1,12 +1,11 @@
 def_EHelper(lui) {
   rtl_li(s, ddest, id_src1->imm);
 }
+
 def_EHelper(auipc){
 	rtl_addi(s,id_dest->preg,&s->pc,id_src1->imm);
 }
-def_EHelper(addi){
-	rtl_addi(s,id_dest->preg,id_src1->preg,id_src2->imm);
-}
+
 def_EHelper(jal){
 	rtl_mv(s,id_dest->preg,&s->snpc);
 	rtl_j(s,id_src1->imm+s->pc);
@@ -17,52 +16,58 @@ def_EHelper(jalr){
 	rtl_andi(s,t0,t0,0xFFFFFFFE);
 	rtl_jr(s,t0);
 }
-def_EHelper(add){
-	rtl_add(s,id_dest->preg,id_src1->preg,id_src2->preg);
+
+//imm&&R
+
+#define def_CYF_REG(name) def_EHelper(name){\
+	concat(rtl_,name)(s,id_dest->preg,id_src1->preg,id_src2->preg);\
 }
-def_EHelper(sub){
-	rtl_sub(s,id_dest->preg,id_src1->preg,id_src2->preg);
+
+#define def_CYF_IMM(name) def_EHelper(name){\
+	concat(rtl_,name)(s,id_dest->preg,id_src1->preg,id_src2->imm);\
 }
-def_EHelper(sltiu){
-	rtl_setrelopi(s,RELOP_LTU,id_dest->preg,id_src1->preg,id_src2->imm);
+
+#define def_CYF_ALL(name) def_CYF_REG(name)\
+	def_CYF_IMM(concat(name,i))
+
+def_CYF_ALL(add)
+def_CYF_REG(sub)//Attention! I-type do not has the instr subi.
+def_CYF_ALL(xor)
+def_CYF_ALL(or)
+def_CYF_ALL(sll)
+def_CYF_ALL(srl)
+def_CYF_ALL(sra)
+def_CYF_ALL(and)
+
+#define def_CYF_REG2(name,type) def_EHelper(name){\
+	rtl_setrelop(s,concat(RELOP_,type),id_dest->preg,id_src1->preg,id_src2->preg);\
 }
-def_EHelper(beq){
-	rtl_jrelop(s,RELOP_EQ,id_src1->preg,id_src2->preg,s->pc+id_dest->imm);
+
+#define def_CYF_IMM2(name,type) def_EHelper(name){\
+	rtl_setrelopi(s,concat(RELOP_,type),id_dest->preg,id_src1->preg,id_src2->imm);\
 }
-def_EHelper(bne){
-	rtl_jrelop(s,RELOP_NE,id_src1->preg,id_src2->preg,s->pc+id_dest->imm);
+
+#define def_CYF_ALL2(name,type) def_CYF_REG2(name,type)\
+	def_CYF_IMM2(concat(name,i),type)
+
+def_CYF_ALL2(slt,LT)
+def_CYF_ALL2(sltu,LTU)
+
+//B
+
+#define def_CYF_B(name,type) def_EHelper(name){\
+	rtl_jrelop(s,concat(RELOP_,type),id_src1->preg,id_src2->preg,s->pc+id_dest->imm);\
 }
-def_EHelper(sltu){
-	rtl_setrelop(s,RELOP_LTU,id_dest->preg,id_src1->preg,id_src2->preg);
-}
-def_EHelper(xor){
-	rtl_xor(s,id_dest->preg,id_src1->preg,id_src2->preg);
-}
-def_EHelper(or){
-	rtl_or(s,id_dest->preg,id_src1->preg,id_src2->preg);
-}
-def_EHelper(srai){
-	rtl_srai(s,id_dest->preg,id_src1->preg,id_src2->imm);
-}
-def_EHelper(sra){
-	rtl_srai(s,id_dest->preg,id_src1->preg,id_src2->imm);
-}
-def_EHelper(blt){
-	rtl_jrelop(s,RELOP_LT,id_src1->preg,id_src2->preg,s->pc+id_dest->imm);
-}
-def_EHelper(bge){
-	rtl_jrelop(s,RELOP_GE,id_src1->preg,id_src2->preg,s->pc+id_dest->imm);
-}
-def_EHelper(bltu){
-	rtl_jrelop(s,RELOP_LTU,id_src1->preg,id_src2->preg,s->pc+id_dest->imm);
-}
-def_EHelper(bgeu){
-	rtl_jrelop(s,RELOP_GEU,id_src1->preg,id_src2->preg,s->pc+id_dest->imm);
-}
-def_EHelper(slti){
-	rtl_setrelopi(s,RELOP_LT,id_dest->preg,id_src1->preg,id_src2->imm);
-}
-def_EHelper(xori){
+
+def_CYF_B(beq,EQ)
+def_CYF_B(bne,NE)
+def_CYF_B(blt,LT)
+def_CYF_B(bge,GE)
+def_CYF_B(bltu,LTU)
+def_CYF_B(bgeu,GEU)
+
+//Codes below are replaced by macro. Not deleted.
+/*def_EHelper(xori){
 	rtl_xori(s,id_dest->preg,id_src1->preg,id_src2->imm);
 }
 def_EHelper(ori){
@@ -76,5 +81,53 @@ def_EHelper(slli){
 }
 def_EHelper(srli){
 	rtl_srli(s,id_dest->preg,id_src1->preg,id_src2->imm);
+}*/
+/*def_EHelper(addi){
+	rtl_addi(s,id_dest->preg,id_src1->preg,id_src2->imm);
+}*/
+/*def_EHelper(add){
+	rtl_add(s,id_dest->preg,id_src1->preg,id_src2->preg);
 }
+def_EHelper(sub){
+	rtl_sub(s,id_dest->preg,id_src1->preg,id_src2->preg);
+}*/
+/*def_EHelper(xor){
+	rtl_xor(s,id_dest->preg,id_src1->preg,id_src2->preg);
+}
+def_EHelper(or){
+	rtl_or(s,id_dest->preg,id_src1->preg,id_src2->preg);
+}
+def_EHelper(srai){
+	rtl_srai(s,id_dest->preg,id_src1->preg,id_src2->imm);
+}
+def_EHelper(sra){
+	rtl_sra(s,id_dest->preg,id_src1->preg,id_src2->preg);
+}*/
+/*def_EHelper(sltiu){
+	rtl_setrelopi(s,RELOP_LTU,id_dest->preg,id_src1->preg,id_src2->imm);
+}*/
+/*def_EHelper(sltu){
+	rtl_setrelop(s,RELOP_LTU,id_dest->preg,id_src1->preg,id_src2->preg);
+}*/
+/*def_EHelper(slti){
+	rtl_setrelopi(s,RELOP_LT,id_dest->preg,id_src1->preg,id_src2->imm);
+}*/
+/*def_EHelper(beq){
+	rtl_jrelop(s,RELOP_EQ,id_src1->preg,id_src2->preg,s->pc+id_dest->imm);
+}
+def_EHelper(bne){
+	rtl_jrelop(s,RELOP_NE,id_src1->preg,id_src2->preg,s->pc+id_dest->imm);
+}
+def_EHelper(blt){
+	rtl_jrelop(s,RELOP_LT,id_src1->preg,id_src2->preg,s->pc+id_dest->imm);
+}
+def_EHelper(bge){
+	rtl_jrelop(s,RELOP_GE,id_src1->preg,id_src2->preg,s->pc+id_dest->imm);
+}
+def_EHelper(bltu){
+	rtl_jrelop(s,RELOP_LTU,id_src1->preg,id_src2->preg,s->pc+id_dest->imm);
+}
+def_EHelper(bgeu){
+	rtl_jrelop(s,RELOP_GEU,id_src1->preg,id_src2->preg,s->pc+id_dest->imm);
+}*/
 
