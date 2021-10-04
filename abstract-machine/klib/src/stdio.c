e382d9ded0e8140f3b86af593600b1ab4a8d8e36
tracer-ics2021 <tracer@njuics.org>
2021-10-05 04:39:13 +0800

    >  compile 201220073    Static hostname: cao-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c            Boot ID: cb7b08a6047e4f4390c1563848828649     Virtualization: oracle   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  04:39:13 up 10:35,  1 user,  load average: 0.00, 0.09, 0.09

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,7 +4,15 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-
+static char* num_to_str(char *st,int d){
+	if(d==0){*st++='0';return st;}
+	if(d<0){*st++='-';d=-d;}
+	char temp[30];
+	int top=0;
+	for(;d;d/=10) temp[++top]=d%10+'0';
+	while(top) *st++=temp[top--];
+	return st;
+}
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
@@ -23,9 +31,7 @@ int sprintf(char *out, const char *fmt, ...) {
 						for(char *ss=s;*ss;++ss,++st) *st=*ss;
 						break;
 				case 'd':d=va_arg(ap,int);
-						if(d==0){*st++='0';break;}
-						if(d<0){*st++='-';d=-d;}
-						for(;d;d/=10) *st++=d%10+'0';
+						st=num_to_str(st,d);
 						break;
 				case 'c':c=(char)va_arg(ap,int);
 						*st++=c;
