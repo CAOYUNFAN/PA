f97389d3cce8db4f8a5ec7bba8587169661cf6c4
tracer-ics2021 <tracer@njuics.org>
2021-10-05 03:44:49 +0800

    >  compile 201220073    Static hostname: cao-VirtualBox          Icon name: computer-vm            Chassis: vm         Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c            Boot ID: cb7b08a6047e4f4390c1563848828649     Virtualization: oracle   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  03:44:49 up  9:41,  1 user,  load average: 0.31, 0.16, 0.11

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -13,13 +13,13 @@ size_t strlen(const char *s) {
 char *strcpy(char *dst, const char *src) {
 	size_t i;
 	for (i = 0;src[i] != '\0'; i++)
-	   dst[i] = src[i];   
+		dst[i] = src[i];   
 	dst[i] = '\0';
 	return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  size_t i;
+	size_t i;
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dst[i] = src[i];
 	for ( ; i < n; i++)
@@ -30,17 +30,14 @@ char *strncpy(char *dst, const char *src, size_t n) {
 char *strcat(char *dst, const char *src) {
 	size_t dest_len = strlen(dst);
 	size_t i;
-
 	for (i = 0 ; src[i] != '\0' ; i++)
-		 dst[dest_len + i] = src[i];
+		dst[dest_len + i] = src[i];
 	dst[dest_len + i] = '\0';
-
 	return dst;
-
 }
 
 int strcmp(const char *s1, const char *s2) {
-  int l1=strlen(s1),l2=strlen(s2);
+	int l1=strlen(s1),l2=strlen(s2);
 	int n=l1<l2?l1:l2;
 	for(int i=0;i<n;++i) if(*(s1+i)!=*(s2+i))
 		return *(s1+i)<*(s2+i)?-1:1;
@@ -59,8 +56,8 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 
 void *memset(void *s, int c, size_t n) {
 	unsigned char uc=c;
-  for(unsigned char *ss=s;n;--n,++ss) *ss=uc;
-  return s;
+	for(unsigned char *ss=s;n;--n,++ss) *ss=uc;
+	return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
