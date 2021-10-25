187f042a19d900a4044ef60df3657b5cb7c4ed72
tracer-ics2021 <tracer@njuics.org>
2021-10-25 23:27:12 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 0c720ced28a141ddbb441064b3cada0f   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  23:27:12 up  3:31,  1 user,  load average: 1.13, 0.84, 0.58

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -4,8 +4,8 @@
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if(ref_r->pc!=pc) return false;
- // for(int i=0;i<32;++i)
- // if(ref_r->gpr[i]!=gpr(i)) return false;
+  for(int i=0;i<32;++i)
+  if(ref_r->gpr[i]._32!=gpr(i)) return false;
   return true;
 }
 
