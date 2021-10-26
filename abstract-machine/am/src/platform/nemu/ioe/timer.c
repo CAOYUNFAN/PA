03e3d987cc8100a6b9e9b55dceb45785ca60740c
tracer-ics2021 <tracer@njuics.org>
2021-10-26 11:54:48 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 585eb5146ecc4b18909a3d553b55a785   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  11:54:48 up  1:29,  1 user,  load average: 0.06, 0.22, 0.24

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,9 +5,8 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  uptime->us = ((uint64_t)inl(RTC_ADDR+4)<<32uLL)+(uint64_t)inl(RTC_ADDR);
 }
-
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
   rtc->second = 0;
   rtc->minute = 0;
--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,7 +4,8 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-static char* num_to_str(char *st,int d){
+static const char num_table[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+inline static char* num_to_str(char *st,int d){
 	if(d==0){*st++='0';return st;}
 	if(d<0){*st++='-';d=-d;}
 	char temp[30];
@@ -13,17 +14,29 @@ static char* num_to_str(char *st,int d){
 	while(top) *st++=temp[top--];
 	return st;
 }
+
+inline static char* unum_to_str(char *st,unsigned int d,int base){
+	if(d==0){*st++='0';return st;}
+	char temp[30];
+	int top=0;
+	for(;d;d/=base) temp[++top]=num_table[d%base];
+	while(top) *st++=temp[top--];
+	return st;
+}
+
+#define MAX_NUM_stdio 1000
+static char temp[MAX_NUM_stdio];
+
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+	va_list ap; va_start(ap,fmt);
+	int t=vsprintf(temp,fmt,ap);
+	va_end(ap);
+	putstr(temp);
+	return t;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
-}
-
-int sprintf(char *out, const char *fmt, ...) {
-	va_list ap; va_start(ap,fmt);
-	int d;char c;char *st=out;char* s;
+  	int d;char c;char *st=out;char* s;
 	while(*fmt){
 		if(*fmt=='%'){
 			switch(*(++fmt)){
@@ -35,6 +48,17 @@ int sprintf(char *out, const char *fmt, ...) {
 						break;
 				case 'c':c=(char)va_arg(ap,int);
 						*st++=c;
+						break;
+				case '%':*st++='%';
+						break;
+				case 'u':d=va_arg(ap,int);
+						st=unum_to_str(st,(unsigned)d,10);
+						break;
+				case 'o':d=va_arg(ap,int);
+						st=unum_to_str(st,(unsigned)d,8);
+						break;
+				case 'x':d=va_arg(ap,int);
+						st=unum_to_str(st,(unsigned)d,16);
 						break;
 				default:panic("Not implemented or error happens");
 			}
@@ -45,33 +69,24 @@ int sprintf(char *out, const char *fmt, ...) {
 	return st-out;
 }
 
+int sprintf(char *out, const char *fmt, ...) {
+	va_list ap; va_start(ap,fmt);
+	int temp=vsprintf(out,fmt,ap);
+	va_end(ap);
+	return temp;
+}
+
 int snprintf(char *out, size_t n, const char *fmt, ...) {
 	va_list ap; va_start(ap,fmt);
-	int d;char c;char *st=out;char* s;
-	while(*fmt){
-		if(*fmt=='%'){
-			switch(*(++fmt)){
-				case 's': s=va_arg(ap,char *);
-						for(char *ss=s;*ss;++ss,++st) *st=*ss;
-						break;
-				case 'd':d=va_arg(ap,int);
-						st=num_to_str(st,d);
-						break;
-				case 'c':c=(char)va_arg(ap,int);
-						*st++=c;
-						break;
-				default:panic("Not implemented or error happens");
-			}
-			++fmt;
-		}else *st++=*fmt++;
-	}
-	if(st-out<n) *st='\0';
-	else out[n-1]='\0';
-	return st-out;
+	int temp=vsnprintf(out,n,fmt,ap);
+	va_end(ap);
+	return temp;
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+	int temp=vsprintf(out,fmt,ap);
+	if(temp>=n) out[n-1]='\0';
+	return temp;
 }
 
 #endif
