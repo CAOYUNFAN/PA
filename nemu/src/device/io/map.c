109d15e3bf1e6b0eafdc4507efbbc84e1a98fd65
tracer-ics2021 <tracer@njuics.org>
2021-10-27 00:50:22 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 585eb5146ecc4b18909a3d553b55a785   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  00:50:22 up 11:46,  1 user,  load average: 0.60, 0.49, 0.41

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -43,6 +43,9 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
+#ifdef CONFIG_DTRACE
+  Log("Read Device Addr=%u ,Device=%s ,len=%d ,Data= %u",addr,map->name,len,ret);
+#endif
   return ret;
 }
 
@@ -51,5 +54,8 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
+#ifdef CONFIG_DTRACE
+  Log("Write Device Addr=%u ,Device=%s ,len=%d ,Data= %u",addr,map->name,len,data);
+#endif
   invoke_callback(map->callback, offset, len, true);
 }
