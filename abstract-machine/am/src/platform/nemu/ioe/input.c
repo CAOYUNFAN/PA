6da4fc74367133a92e054bc4a82af7f18e6ab7d1
tracer-ics2021 <tracer@njuics.org>
2021-10-27 01:16:39 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 585eb5146ecc4b18909a3d553b55a785   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  01:16:39 up 12:12,  1 user,  load average: 0.29, 0.37, 0.41

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,8 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  static bool keyboard_status[AM_KEY_PAGEDOWN+1]={};
+  uint32_t temp=inl(KBD_ADDR);
+  kbd->keydown = (keyboard_status[temp]=!keyboard_status[temp]);
+  kbd->keycode = temp;
 }
