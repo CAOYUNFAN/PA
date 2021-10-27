1c540e8165c40973de71a24a2fa2b673c58211d2
tracer-ics2021 <tracer@njuics.org>
2021-10-27 14:44:30 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 9458852113fb4ebdb69b8a1e6e8314a1   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  14:44:30 up  2:13,  1 user,  load average: 0.83, 0.41, 0.38

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -2,18 +2,20 @@
 #include <nemu.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
+#define wide (uint32_t)inw(VGACTL_ADDR+2)
+#define high (uint32_t)inw(VGACTL_ADDR)
 
 void __am_gpu_init() {
   int i;
-  int w = (int)inw(VGACTL_ADDR+2);  // TODO: get the correct width
-  int h = (int)inw(VGACTL_ADDR);  // TODO: get the correct height
+  int w = (int)wide;  // TODO: get the correct width
+  int h = (int)high;  // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  uint32_t w=(uint32_t)inw(VGACTL_ADDR+2),h=(uint32_t)inw(VGACTL_ADDR);
+  uint32_t w=wide,h=high;
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
     .width = w, .height = h,
@@ -22,6 +24,10 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  size_t *data=ctl->pixels;
+  for(int i=0;i<ctl->h;++i)
+  for(int j=0;j<ctl->w;++j)
+  outl((ctl->x+i)*wide+(ctl->y+j)+FB_ADDR,*(data++));
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
