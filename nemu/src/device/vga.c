fb62c6606d9ea9b48fc00356a0b5a7fd0635a976
tracer-ics2021 <tracer@njuics.org>
2021-10-27 13:28:26 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 9458852113fb4ebdb69b8a1e6e8314a1   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  13:28:26 up  1:26,  1 user,  load average: 0.14, 0.27, 0.32

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -58,10 +58,9 @@ static inline void update_screen() {
 void vga_update_screen() {
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
-  #define sync vgactl_port_base[1]
-  if(sync){
+  if(vgactl_port_base[1]){
     update_screen();
-    sync=0;
+    vgactl_port_base[1]=0;
   }
 }
 
