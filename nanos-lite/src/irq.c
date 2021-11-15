3d3654d468bfb13e8364f9974e0d9308cfe2854c
tracer-ics2021 <tracer@njuics.org>
2021-11-16 03:06:13 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 45b662cf47c34fa6bf369446d4f10553   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  03:06:13 up  4:32,  1 user,  load average: 0.36, 0.36, 0.31

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -14,7 +14,7 @@ void event_yield(Context *c){
 
 static Context* do_event(Event e, Context* c) {
   #ifdef CONFIG_ETRACE
-  Log("Event ID=%2d,mcause=%2d,from pc=%8x\n",e.event,c->mcause,c->mepc);
+  Log("Event ID=%2d,mcause=%2d,from pc=%8x",e.event,c->mcause,c->mepc);
   #endif
   switch (e.event) {
     case EVENT_YIELD: event_yield(c);break;
