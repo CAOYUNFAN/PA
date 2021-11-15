7fa8afbd66e2b5cef31008df0f6e38f257dfdd47
tracer-ics2021 <tracer@njuics.org>
2021-11-16 02:38:34 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 45b662cf47c34fa6bf369446d4f10553   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  02:38:34 up  4:07,  1 user,  load average: 0.25, 0.36, 0.31

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,7 +1,19 @@
 #include <common.h>
 
+void event_yield(){
+  printf("EVENT_YIELD!\n");
+  return;
+}
+
+/*enum event_type{
+  EVENT_NULL = 0,
+  EVENT_YIELD, EVENT_SYSCALL, EVENT_PAGEFAULT, EVENT_ERROR,
+  EVENT_IRQ_TIMER, EVENT_IRQ_IODEV,
+};*/
+
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
+    case EVENT_YIELD: event_yield();break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
