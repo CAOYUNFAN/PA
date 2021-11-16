9087b80613b8811f62ade170599d6deed93617db
tracer-ics2021 <tracer@njuics.org>
2021-11-16 22:39:59 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: a0c3b0f6a4d44ef28c8e212cef5cd546   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  22:39:59 up  2:23,  1 user,  load average: 0.34, 0.22, 0.25

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,8 +9,40 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+void check_elf(const Elf_Ehdr *ehdr){
+  assert(ehdr->e_ident[EI_MAG0]==0x7f&&ehdr->e_ident[EI_MAG1]=='E');
+  assert(ehdr->e_ident[EI_MAG2]=='L'&&ehdr->e_ident[EI_MAG3]=='F');
+  #ifdef __LP64__
+  assert(ehdr->e_ident[EI_CLASS]==ELFCLASS64);
+  #else
+  assert(ehdr->e_ident[EI_CLASS]==ELFCLASS32);
+  #endif
+  
+  #if defined(__ISA_AM_NATIVE__)
+  # define EXPECT_TYPE EM_X86_64
+  #elif defined(__ISA_X86__)
+  # define EXPECT_TYPE EM_386
+  #elif defined(__ISA_MIPS32__)
+  # define EXPECT_TYPE EM_MIPS_X
+  #elif defined(__ISA_RISCV32__)
+  # define EXPECT_TYPE EM_RISCV
+  #elif defined(__ISA_RISCV64__)
+  # define EXPECT_TYPE EM_RISCV
+  #else
+  # error Unsupported ISA
+  #endif
+  assert(ehdr->e_machine==EXPECT_TYPE);
+  return;
+}
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
+  Elf_Ehdr ehdr;
+  extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+  extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+  ramdisk_read(&ehdr,0,sizeof(ehdr));
+  check_elf(&ehdr);
+  
+
   return 0;
 }
 
