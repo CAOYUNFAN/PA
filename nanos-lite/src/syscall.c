856bd36a69ddd0166ae51f3983b7fc9f5c66f759
tracer-ics2021 <tracer@njuics.org>
2021-11-19 03:37:56 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: bcf6f419074e4bf3b0ef09083ab8ea28   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  03:37:56 up  2:02,  1 user,  load average: 0.35, 0.48, 0.45

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,29 @@
 #include <common.h>
 #include "syscall.h"
+
+enum SYS_type{
+  SYS_exit,
+  SYS_yield,
+  SYS_open,
+  SYS_read,
+  SYS_write,
+  SYS_kill,
+  SYS_getpid,
+  SYS_close,
+  SYS_lseek,
+  SYS_brk,
+  SYS_fstat,
+  SYS_time,
+  SYS_signal,
+  SYS_execve,
+  SYS_fork,
+  SYS_link,
+  SYS_unlink,
+  SYS_wait,
+  SYS_times,
+  SYS_gettimeofday
+};
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -9,7 +33,7 @@ void do_syscall(Context *c) {
   extern void event_yield(Context *c);
 
   switch (a[0]) {
-    case 1: event_yield(c);break;
+    case SYS_yield: event_yield(c);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
