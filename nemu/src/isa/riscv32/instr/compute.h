a1e094a8b70f767f1ae0030b90b9fcfefc709a0c
tracer-ics2021 <tracer@njuics.org>
2021-11-19 19:56:10 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: cc1bcaa6b54145c7bf11690bba9aa164   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  19:56:10 up 30 min,  1 user,  load average: 0.52, 0.37, 0.42

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -12,9 +12,8 @@ def_EHelper(jal){
 }
 def_EHelper(jalr){
 	rtl_mv(s,id_dest->preg,&s->snpc);
-	rtl_addi(s,t0,id_src1->preg,id_src2->imm);
-	rtl_andi(s,t0,t0,0xFFFFFFFE);
-	rtl_jr(s,t0);
+	rtl_addi(s,&s->dnpc,id_src1->preg,id_src2->imm);
+	rtl_andi(s,&s->dnpc,&s->dnpc,0xFFFFFFFE);
 }
 
 //imm&&R
