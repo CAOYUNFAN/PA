1f8bd465048b8348526ffd069f9de5f4985ed3b5
tracer-ics2021 <tracer@njuics.org>
2021-11-25 22:53:46 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: dcbc9b5462f54e02b13244b62918ddb7   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  22:53:46 up  4:50,  1 user,  load average: 0.50, 0.47, 0.37

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -71,6 +71,7 @@ void do_syscall(Context *c) {
     case SYS_open: c->GPRx=fs_open((void *)a[1],a[2],a[3]); break;
     case SYS_close: c->GPRx=fs_close(a[1]); break;
     case SYS_read: c->GPRx=sys_read(a[1],(void *)a[2],a[3]);
+    case SYS_lseek: c->GPRx=fs_lseek(a[1],a[2],a[3]);
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
