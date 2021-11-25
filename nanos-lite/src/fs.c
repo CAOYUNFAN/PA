bb72755d8318a858317da4d6db685c4265a4b290
tracer-ics2021 <tracer@njuics.org>
2021-11-25 16:54:56 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: dcbc9b5462f54e02b13244b62918ddb7   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  16:54:56 up 33 min,  1 user,  load average: 0.25, 0.25, 0.29

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -34,3 +34,11 @@ static Finfo file_table[] __attribute__((used)) = {
 void init_fs() {
   // TODO: initialize the size of /dev/fb
 }
+
+/*int fs_open(const char *pathname, int flags, int mode){
+  open();
+}*/
+
+int fs_close(int fd){
+  return 0;
+}
