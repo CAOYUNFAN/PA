0e84cc690264e11b8624e37a2ef75df18e3d94ad
tracer-ics2021 <tracer@njuics.org>
2021-11-26 23:24:56 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: b67a7b3fae80448a8044b14672e99fb8   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  23:24:56 up 29 min,  1 user,  load average: 0.35, 0.52, 0.52

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -14,8 +14,8 @@ int main() {
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
-    printf("%d %d\n",n,i);
-  //  assert(n == i + 1);
+  //  printf("%d %d\n",n,i);
+    assert(n == i + 1);
   }
 
   fseek(fp, 0, SEEK_SET);
