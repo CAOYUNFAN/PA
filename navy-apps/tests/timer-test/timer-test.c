c7101f8bb24617ba0ce3e2cdb4107248b9fb2385
tracer-ics2021 <tracer@njuics.org>
2021-11-27 02:18:11 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: b67a7b3fae80448a8044b14672e99fb8   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  02:18:11 up  2:13,  1 user,  load average: 1.10, 0.69, 0.54

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -1,18 +1,18 @@
 #include<unistd.h>
 #include<stdio.h>
 #include<sys/time.h>
+#include<NDL.h>
 
 int main(){
-  struct timeval tv;
-  struct timezone tz;
-  gettimeofday(&tv,&tz);
-  __suseconds_t temp=tv.tv_sec;
+  NDL_Init(0);
+  uint32_t old=NDL_GetTicks();
   while (1)  {
-    gettimeofday(&tv,&tz);
     /*if(tv.tv_usec-500>=0&&temp==1||tv.tv_usec-500<0&&temp==0){
       printf("%ld seconds, %ld microseconds passed!\n",tv.tv_sec,tv.tv_usec);
       temp=1-temp;
     }*/
-    if(temp!=tv.tv_sec) printf("%ld seconds, %ld microseconds passed!\n",tv.tv_sec,tv.tv_usec),temp=tv.tv_sec;
+    uint32_t now=NDL_GetTicks();
+    if(now-old>=500) printf("%u microseconds passed!\n",now),old=now;
   }
+  return 0;
 }
