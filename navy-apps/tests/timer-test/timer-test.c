3b210dcb5a094bc692233077e45524544f2fe145
tracer-ics2021 <tracer@njuics.org>
2021-11-27 01:57:26 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: b67a7b3fae80448a8044b14672e99fb8   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  01:57:26 up  1:52,  1 user,  load average: 0.45, 0.46, 0.49

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -6,9 +6,12 @@ int main(){
   struct timeval tv;
   struct timezone tz;
   gettimeofday(&tv,&tz);
-  __suseconds_t temp=500;
+  __suseconds_t temp=0;
   while (1)  {
     gettimeofday(&tv,&tz);
-    if(tv.tv_usec==500-temp) printf("%ld seconds, %ld microseconds passed!\n",tv.tv_sec,tv.tv_usec),temp=500-temp;
+    if(tv.tv_usec-500>=0&&temp==1||tv.tv_usec-500<0&&temp==0){
+      printf("%ld seconds, %ld microseconds passed!\n",tv.tv_sec,tv.tv_usec);
+      temp=1-temp;
+    }
   }
 }
