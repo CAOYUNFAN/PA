c8285c2766def4153dd7e1608abb1faa370ba5a7
tracer-ics2021 <tracer@njuics.org>
2021-12-03 02:04:51 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 5a19c8c787444ad8a2f2d228e4be17fc   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  02:04:51 up  1:44,  1 user,  load average: 0.71, 0.59, 0.55

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -36,7 +36,7 @@ static struct {
 #define NR_CMD sizeof(cmd_table)/sizeof(cmd_table[0])
 
 static void sh_handle_cmd(const char *cmd) {
-  int i;
+/*  int i;
   char * cmd_=strtok(cmd," ");
   for(i=0;i<NR_CMD;++i)
   if(strcmp(cmd_,cmd_table[i].name)==0){
@@ -44,7 +44,7 @@ static void sh_handle_cmd(const char *cmd) {
     return;
   }
   sh_printf("Unkown or Not Handled Command '%s' :-(\n",cmd_);
-  return;
+  return;*/
 }
 
 void builtin_sh_run() {
