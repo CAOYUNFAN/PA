1967ba286ddd3295c529d9e24ad881a5e54c3306
tracer-ics2021 <tracer@njuics.org>
2021-12-04 03:41:31 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 03836690c11d4d388e8971d570d168ff   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  03:41:31 up  1:20,  1 user,  load average: 0.32, 0.62, 0.54

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -78,7 +78,7 @@ int fs_open(const char *pathname, int flags, int mode){
   for(int i=0;i<total_size;++i)
   if(strcmp(file_table[i].name,pathname)==0){
     file_table[i].open_offset=0;
-//    Log("File %s,id %d,size %u, offset %u",file_table[i].name,i,file_table[i].size,file_table[i].disk_offset);
+    Log("File %s,id %d,size %u, offset %u",file_table[i].name,i,file_table[i].size,file_table[i].disk_offset);
     return i;
   }
   panic("File %s not found!",pathname);
@@ -94,7 +94,7 @@ int fs_close(int fd){
 size_t fs_read(int fd, void *buf, size_t len){
   check_files(fd);
   if(file_table[fd].size) check_filerange(fd,&len);
-//  Log("Read from file %s,with len %u,now %u",file_table[fd].name,len,file_table[fd].disk_offset+file_table[fd].open_offset);
+  Log("Read from file %s,with len %u,now %u",file_table[fd].name,len,file_table[fd].disk_offset+file_table[fd].open_offset);
   if(file_table[fd].read!=NULL){
     size_t temp=file_table[fd].read(buf,file_table[fd].open_offset,len);
     file_table[fd].open_offset+=temp;
