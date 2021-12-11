56bedfbedc3a30e9679866a84b9da0862bdb110f
tracer-ics2021 <tracer@njuics.org>
2021-12-12 04:39:49 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 45ac7f2607864fb49d72e1d503e7bc6c   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  04:39:49 up  5:04,  1 user,  load average: 0.64, 0.50, 0.63

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -84,7 +84,7 @@ static void sh_handle_cmd(const char *cmd) {
     char * ndd=strtok(NULL," ");
 //    printf("%s\n",ndd);
     char ** my=args(ndd);
-    *(NULL)=0;
+    *((int *)0)=0;
     check(filename,my);
     printf("%p %p\n",my,*my);
     if(execv(filename,my)==-1) sh_printf("Program do not exist!\n");
