f27785b5caf9d32e293cf8ae09d1fbf17e5e082f
tracer-ics2021 <tracer@njuics.org>
2021-12-12 01:21:22 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 45ac7f2607864fb49d72e1d503e7bc6c   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  01:21:22 up  2:23,  1 user,  load average: 0.49, 0.52, 0.49

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -1,12 +1,12 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <assert.h>
-//#include <stdio.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-//  printf("%p\n",args);
+  printf("%p\n",args);
   int argc=(int)*args;
   char ** argv=(char **)(args+1);
   char ** envp=(char **)(args+argc+2);
