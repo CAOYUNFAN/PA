17c1d6b1d836f2fce6fa9cafaa5c083bd0f49220
tracer-ics2021 <tracer@njuics.org>
2021-12-16 21:21:48 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 92dd8d9b47144afebe46540c08aa338b   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  21:21:48 up 25 min,  1 user,  load average: 0.39, 0.18, 0.11

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -3,7 +3,7 @@
 #include <assert.h>
 #include <string.h>
 #include <stdlib.h>
-//#include <stdio.h>
+#include <stdio.h>
 //#include <sys/unistd.h>
 
 static inline int Min(int a,int b){return a<b?a:b;}
@@ -17,8 +17,8 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
   if(dstrect==NULL) dx=dy=0;
   else dx=dstrect->x,dy=dstrect->y;
   
-//   printf("%d %d %d %d %d %d\n",sx,sy,sw,sh,dx,dy);
-//  printf("%d %d %d %d\n",src->w,src->h,dst->w,dst->h);
+  printf("%d %d %d %d %d %d\n",sx,sy,sw,sh,dx,dy);
+  printf("%d %d %d %d\n",src->w,src->h,dst->w,dst->h);
   unsigned char * d=(unsigned char *)dst->pixels;
   unsigned char * s=(unsigned char *)src->pixels;
   int pp=dst->format->BytesPerPixel;
