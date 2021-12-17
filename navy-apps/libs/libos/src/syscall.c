007db4b9a543d2e8c0b271804a7d44429fd65752
tracer-ics2021 <tracer@njuics.org>
2021-12-18 03:13:40 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 0a7cae9dd9e249e798888a5c4170a995   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  03:13:40 up  8:20,  1 user,  load average: 0.64, 0.60, 0.65

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -60,9 +60,10 @@ int _open(const char *path, int flags, mode_t mode) {
 //  printf("open file %s\n",path);
   return _syscall_(SYS_open,(intptr_t)path,flags,mode);
 }
-
+#include <stdio.h>
 int _write(int fd, void *buf, size_t count) {
-  return _syscall_(SYS_write,fd,(intptr_t)buf,count);
+  int ret=_syscall_(SYS_write,fd,(intptr_t)buf,count);printf("%d\n",ret);
+  return ret;
 }
 
 void *_sbrk(intptr_t increment) {
