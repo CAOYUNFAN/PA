92d8c7a3d4cf682a7153d266a823f7170b4862de
tracer-ics2021 <tracer@njuics.org>
2021-12-18 05:54:15 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 0a7cae9dd9e249e798888a5c4170a995   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  05:54:15 up  9:56,  1 user,  load average: 0.76, 0.50, 0.46

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -6,21 +6,22 @@ word_t vaddr_ifetch(vaddr_t addr, int len) {
 }
 
 word_t vaddr_read(vaddr_t addr, int len) {
-  switch(isa_mmu_check(addr,len,0)){
+/*  switch(isa_mmu_check(addr,len,0)){
     case MMU_TRANSLATE:
       addr=isa_mmu_translate(addr,len,0);
-    case MMU_DIRECT:
+    case MMU_DIRECT:*/
       return paddr_read(addr,len);
-    default: assert(false);
-  }
+//    default: assert(false);
+//  }
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
-  switch(isa_mmu_check(addr,len,0)){
-    case MMU_TRANSLATE:
-      addr=isa_mmu_check(addr,len,0);
-    case MMU_DIRECT:
-      paddr_write(addr,len,0);break;
-    default: assert(false);
-  }
+//  switch(isa_mmu_check(addr,len,0)){
+//    case MMU_TRANSLATE:
+//      addr=isa_mmu_check(addr,len,0);
+//    case MMU_DIRECT:
+      paddr_write(addr,len,0);
+//      break;
+//    default: assert(false);
+//  }
 }
