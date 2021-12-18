b62239636bc7945eef75c6d4c513eb03418a93dc
tracer-ics2021 <tracer@njuics.org>
2021-12-18 21:27:35 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 2a91b038bba543d8bf86e22507a8c53b   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  21:27:35 up 21 min,  1 user,  load average: 0.48, 0.50, 0.46

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -11,7 +11,7 @@ int isa_mmu_check(vaddr_t vaddr, int len, int type){
 }
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   assert((vaddr>>12)==((vaddr+len-1)>>12));
-  vaddr_t high=vaddr>>22,low=vaddr>>12;
+  vaddr_t high=vaddr>>22,low=(vaddr>>12)&0x3ff;
   printf("Memory:Virtual=%x,High=%x,Low=%x",vaddr,high,low);
   paddr_t pos1=paddr_read(((satp<<10)|high)<<2,4)&~0xfffu;
   paddr_t pos2=paddr_read(pos1|(low<<2),4)&~0xfffu;
