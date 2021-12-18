a1193ed3bd513a08bd89cb3491acd779e16fceb4
tracer-ics2021 <tracer@njuics.org>
2021-12-18 21:10:18 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 2a91b038bba543d8bf86e22507a8c53b   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  21:10:18 up 4 min,  1 user,  load average: 0.46, 0.56, 0.27

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -16,10 +16,12 @@ word_t vaddr_read(vaddr_t addr, int len) {
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
+  printf("VADDR_write:write to virtual address%8u,len%8d,data%8u",addr,len,data);
   switch(isa_mmu_check(addr,len,0)){
     case MMU_TRANSLATE:
       addr=isa_mmu_translate(addr,len,0);
     case MMU_DIRECT:
+      printf("physical address:%8u\n",addr);
       paddr_write(addr,len,data);
       break;
     default: assert(false);
