f72fbde522a0d4d5dd3f0606f386820df7bcea77
tracer-ics2021 <tracer@njuics.org>
2021-12-19 06:27:40 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 2a91b038bba543d8bf86e22507a8c53b   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  06:27:40 up  4:08,  1 user,  load average: 0.70, 0.44, 0.47

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -8,7 +8,7 @@ const char *regs[] = {
   "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
 
-#define Cao_show_reg(name,data) printf("%7s 0x%08x %10d\n",name,data,data)
+#define Cao_show_reg(name,data) printf("%7s 0x%08x %11d\n",name,data,data)
 
 void isa_reg_display() {
 	for(int i=0;i<32;++i) 
