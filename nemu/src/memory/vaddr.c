b82ca83ce1fc8dad5084345e39f2342cc39c7140
tracer-ics2021 <tracer@njuics.org>
2021-12-19 08:19:12 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 2a91b038bba543d8bf86e22507a8c53b   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  08:19:12 up  5:24,  1 user,  load average: 0.35, 0.52, 0.50

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -2,7 +2,7 @@
 #include <memory/paddr.h>
 
 word_t vaddr_read(vaddr_t addr, int len) {
-  if(addr<0x40000000)Log("%x %d",addr,len);
+  Log("Read%x %d",addr,len);
   switch(isa_mmu_check(addr,len,0)){
     case MMU_TRANSLATE:
       addr=isa_mmu_translate(addr,len,0);
@@ -13,7 +13,7 @@ word_t vaddr_read(vaddr_t addr, int len) {
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
-  if(addr<0x40000000)Log("%x %d",addr,len);
+  Log("Write%x %d",addr,len);
   switch(isa_mmu_check(addr,len,0)){
     case MMU_TRANSLATE:
       addr=isa_mmu_translate(addr,len,0);
