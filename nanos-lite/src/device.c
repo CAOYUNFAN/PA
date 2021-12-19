5a53d0da8ee626ee8a3404219bb276dd5bb5bb12
tracer-ics2021 <tracer@njuics.org>
2021-12-20 02:37:36 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 42cd0fe8eb73410fa60a6d4765b52847   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  02:37:36 up  1:54,  1 user,  load average: 0.78, 0.41, 0.30

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -24,7 +24,7 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 inline static size_t min(size_t a,size_t b){return a<b?a:b;}
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  yield();
+//  yield();
   struct AM_INPUT_KEYBRD_T{
     bool keydown; int keycode;
   }kbd;
@@ -48,7 +48,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
 //  printf("%u %u\n",offset,len);
-  yield();
+//  yield();
   __am_gpu_config(&cfg);
   struct AM_GPU_FBDRAW_T{
     int x, y; const void *pixels; int w, h; bool sync;
