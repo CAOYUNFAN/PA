1b609e91ce9ebd6d5cf1119cfa7a55a16cefb253
tracer-ics2021 <tracer@njuics.org>
2021-12-20 09:38:48 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: ad85ab2c35264359b4325700f668d3de   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  09:38:48 up  1:00,  1 user,  load average: 0.56, 0.38, 0.38

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -46,15 +46,14 @@ int now=-1,cover=-1;
 #endif
 
 static void statistic() {
-//	if(nemu_state.state!=NEMU_END&&nemu_state.state!=NEMU_QUIT){
+	if(nemu_state.state!=NEMU_END&&nemu_state.state!=NEMU_QUIT){
 #ifdef CONFIG_ITRACE
     for(int i=0;i<cover;i++){
   		if(i==now) printf(" --> %s\n",ring_buffer[i]);
 	  	else printf("     %s\n",ring_buffer[i]);
 	  }
 #endif
-    isa_reg_display();
-//  }
+  }
 
   IFNDEF(CONFIG_TARGET_AM, setlocale(LC_NUMERIC, ""));
 #define NUMBERIC_FMT MUXDEF(CONFIG_TARGET_AM, "%ld", "%'ld")
@@ -65,7 +64,7 @@ static void statistic() {
 }
 
 void assert_fail_msg() {
-//  isa_reg_display();
+  isa_reg_display();
   statistic();
 }
 void trace_and_difftest(Decode *s, vaddr_t pc){
