d9ef0613f05c2e9676ce1ca39dce92e1e4d54097
tracer-ics2021 <tracer@njuics.org>
2021-12-21 10:03:29 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 0b19f80c909e43dcb6015cff3f158ddf   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  10:03:29 up  1:36,  1 user,  load average: 0.56, 0.48, 0.42

--- navy-apps/apps/nterm/src/main.cpp
+++ navy-apps/apps/nterm/src/main.cpp
@@ -11,9 +11,8 @@ void builtin_sh_run();
 void extern_app_run(const char *app_path);
 //void env_init();
 
-int main(int argc, char *argv[]) {//printf("A\n");
+int main(int argc, char *argv[]) {printf("A\n");
   SDL_Init(0);
-//  env_init();
   font = new BDF_Font(font_fname);
 
   // setup display
