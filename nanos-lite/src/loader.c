9fb12e4449e3c4ced70a3703ed1b7590a4d25f41
tracer-ics2021 <tracer@njuics.org>
2021-12-21 09:02:46 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 0b19f80c909e43dcb6015cff3f158ddf   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  09:02:46 up 35 min,  1 user,  load average: 1.00, 0.58, 0.44

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -93,7 +93,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {Log("%p %s",pcb,filenam
       uintptr_t virtual_page=phdr.p_vaddr&~0xfffu,offset=phdr.p_vaddr&0xfffu;
       int total1=phdr.p_filesz,total2=phdr.p_memsz-phdr.p_filesz;
 //      pcb->max_brk=Max(pcb->max_brk,phdr.p_vaddr+phdr.p_memsz);Log("%08x",pcb->max_brk);
-
+      Log("%08x To %08x:",phdr.p_vaddr,phdr.p_vaddr+phdr.p_memsz);
       for(;total1;virtual_page+=0x1000u){
         uintptr_t physical_page=get_page(as,virtual_page);
         int bytes=fs_read(fd,(void *)physical_page+offset,Min(pgsize-offset,total1));
