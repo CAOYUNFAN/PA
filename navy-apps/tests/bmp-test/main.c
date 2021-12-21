9abdd36ae45b85e213305d27873485c8147f45bc
tracer-ics2021 <tracer@njuics.org>
2021-12-21 11:40:03 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 0b19f80c909e43dcb6015cff3f158ddf   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  11:40:03 up  2:56,  1 user,  load average: 0.24, 0.50, 0.50

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -7,12 +7,15 @@
 int main() {
   NDL_Init(0);
   int w, h;
-  printf("SB!\n");
   void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
   assert(bmp);
+  printf("DEBUG1!\n");
   NDL_OpenCanvas(&w, &h);
+  printf("DEBUG2!\n");
   NDL_DrawRect(bmp, 0, 0, w, h);
+  printf("DEBUG3!\n");
   free(bmp);
+  printf("DEBUG4!\n");
   NDL_Quit();
   printf("Test ends! Spinning...\n");
   while (1);
