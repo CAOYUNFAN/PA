21c14f3c0e1bd07b64918ca8c3d7a49424c328f2
tracer-ics2021 <tracer@njuics.org>
2021-12-23 04:24:10 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 090a5e133cca4aaeadc0eae59bf5e28f   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  04:24:10 up  1:59,  1 user,  load average: 0.36, 0.33, 0.35

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -45,7 +45,7 @@ void init_proc() {
   char * envp[]={"PATH=/bin/:/usr/bin/",NULL};
   assert(context_uload(&pcb[1],"/bin/bmp-test",argv,envp));
   switch_boot_pcb();
-Log("ildsgjdf");
+
   // load program here
 //  extern void naive_uload(PCB *pcb, const char *filename);
 //  naive_uload(NULL, "/bin/pal");
