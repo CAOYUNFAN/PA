af3d162cf9efa4e24701818795ca4dce1316f030
tracer-ics2021 <tracer@njuics.org>
2021-12-23 02:42:54 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 090a5e133cca4aaeadc0eae59bf5e28f   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  02:42:54 up 18 min,  1 user,  load average: 0.55, 0.50, 0.35

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -4,10 +4,10 @@
 #include <NDL.h>
 #include <BMP.h>
 #include <stdint.h>
-#define SYS_yield 1
-extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
+//#define SYS_yield 1
+//extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 int main() {
-  NDL_Init(0);_syscall_(SYS_yield, 0, 0, 0);
+  NDL_Init(0);//_syscall_(SYS_yield, 0, 0, 0);
   int w, h;
   void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
   assert(bmp);
