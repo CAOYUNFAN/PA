79f2377b1b5db51ab19f7ecaf31368982c3bbbcb
tracer-ics2021 <tracer@njuics.org>
2021-12-23 02:42:14 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 090a5e133cca4aaeadc0eae59bf5e28f   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  02:42:14 up 17 min,  1 user,  load average: 0.32, 0.45, 0.32

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -20,11 +20,11 @@ struct BitmapHeader {
 } __attribute__((packed));
 
 void* BMP_Load(const char *filename, int *width, int *height) {
-  FILE *fp = fopen(filename, "r");
-  if (!fp) return NULL;
-
 #define SYS_yield 1
-extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
+extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);_syscall_(SYS_yield, 0, 0, 0);
+  FILE *fp = fopen(filename, "r");
+  if (!fp) return NULL;_syscall_(SYS_yield, 0, 0, 0);
+
   struct BitmapHeader hdr;
   assert(sizeof(hdr) == 54);
   assert(1 == fread(&hdr, sizeof(struct BitmapHeader), 1, fp));
@@ -32,7 +32,7 @@ extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
   if (hdr.bitcount != 24) return NULL;
   if (hdr.compression != 0) return NULL;
   int w = hdr.width;
-  int h = hdr.height;
+  int h = hdr.height;_syscall_(SYS_yield, 0, 0, 0);
   uint32_t *pixels = malloc(w * h * sizeof(uint32_t));
 
   int line_off = (w * 3 + 3) & ~0x3;
