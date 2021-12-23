1c68e8fb00a276f5b150844f75ec5fe862011475
tracer-ics2021 <tracer@njuics.org>
2021-12-24 04:34:57 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: ad85931b68e04396b5443f0cd4a88fb4   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  04:34:57 up  6:20,  1 user,  load average: 0.39, 0.51, 0.53

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -57,7 +57,7 @@ uintptr_t fg_pcb=1,cycle_num=0;
 
 Context* schedule(Context *prev) {
   current->cp=prev;
-  for(int i=0;i<4;++i) Log("%d:%08x,%08x",i,&pcb[i],pcb[i].cp->pdir);Log("end!%08x",current);assert(pcb[0].cp->pdir==0);
+  for(int i=0;i<4;++i) Log("%d:%08x,%08x,%08x",i,&pcb[i],pcb[i].cp->pdir,pcb[i].cp->np);Log("end!%08x",current);assert(pcb[0].cp->pdir==0);
   if(!cycle_num) current=&pcb[0];
   else current=&pcb[fg_pcb];
   cycle_num=(cycle_num+1)&((1<<3)-1);
