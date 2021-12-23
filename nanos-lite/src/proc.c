b104fd91e12f43a66e6be8f703aabc79b7600c70
tracer-ics2021 <tracer@njuics.org>
2021-12-24 03:18:14 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: ad85931b68e04396b5443f0cd4a88fb4   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  03:18:14 up  5:07,  1 user,  load average: 0.84, 0.46, 0.42

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -42,9 +42,11 @@ char * envp_for_main[]={"PATH=/bin/:/usr/bin/",NULL};
 
 void init_proc() {
   Log("Initializing processes...");
-
+  static char * argv[]={"--skip",NULL};
   context_kload(&pcb[0], hello_fun, "pcb_0");
   assert(context_uload(&pcb[1],main_name,argv_for_main,envp_for_main));
+  assert(context_uload(&pcb[2],"/bin/pal",argv,envp_for_main));
+  assert(context_uload(&pcb[3],"/bin/bird",argv_for_main,envp_for_main));
   switch_boot_pcb();
 
   // load program here
