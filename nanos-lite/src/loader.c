1610648ff16b8aa3d7082cb30624ca6423048123
tracer-ics2021 <tracer@njuics.org>
2021-12-24 03:39:38 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: ad85931b68e04396b5443f0cd4a88fb4   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  03:39:38 up  5:28,  1 user,  load average: 0.44, 0.55, 0.57

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -196,7 +196,7 @@ static char * prepare_args_and_stack(AddrSpace *as,char * const argv[],char * co
 
 bool context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]){
 #ifdef HAS_VME
-  protect(&pcb->as);Log("%10s:%08x",filename,pcb->as.ptr);
+  protect(&pcb->as);Log("%10s:%08x,%p",filename,pcb->as.ptr,pcb);
 #endif
   uintptr_t ret=(uintptr_t)prepare_args_and_stack(&pcb->as,argv,envp);
   Area mystack={
