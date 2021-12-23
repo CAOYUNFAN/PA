0879576142ee66ebb91a4b054265b930fe745a53
tracer-ics2021 <tracer@njuics.org>
2021-12-23 21:34:05 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: ad85931b68e04396b5443f0cd4a88fb4   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  21:34:05 up  3:21,  1 user,  load average: 0.85, 0.74, 0.67

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -5,7 +5,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
    * Then return the address of the interrupt/exception vector.
    */
   cpu.sr[0x341]._32=epc;
-  cpu.sr[0x342]._32=NO;Log("ERRERNO:%08x",NO);
+  cpu.sr[0x342]._32=NO;//Log("ERRERNO:%08x",NO);
   rtlreg_t old=(cpu.sr[0x300]._32>>3)&1;
   cpu.sr[0x300]._32^=(cpu.sr[0x300]._32&((1<<7)|(1<<3)))^(old<<7);
   return cpu.sr[0x305]._32;
