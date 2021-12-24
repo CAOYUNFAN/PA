1bc011141d6768dc97251ad9ec82c397cbff284a
tracer-ics2021 <tracer@njuics.org>
2021-12-24 08:34:45 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 7e2838bef9974513a404eabaa5660de4   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  08:34:45 up 51 min,  1 user,  load average: 0.12, 0.45, 0.50

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -61,7 +61,7 @@ void __am_get_cur_as(Context *c) {//printf("%08x\n",c);
 }
 
 void __am_switch(Context *c) {
-  if (vme_enable) {
+  if (vme_enable&&c->pdir!=NULL) {
     set_satp(c->pdir);
   }
 }
