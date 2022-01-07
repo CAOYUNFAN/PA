9fca2570aa65a9979d2b6c4cf14863fa799ffec1
tracer-ics2021 <tracer@njuics.org>
2022-01-07 12:07:12 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 4939437a4d4a4de69744a3e0ffabbb67   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  12:07:12 up  1:58,  1 user,  load average: 0.93, 0.64, 0.54

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -66,7 +66,6 @@ Context* schedule(Context *prev) {
 
 //  current->cp=prev;
   if(current->cp!=NULL&&current!=&pcb[0]) memcpy(current->cp,prev,sizeof(Context));
-  Log("%08x",pcb[0].cp->gpr[10]);
 //  static int xx=0;++xx;
 
   #ifdef cyf_DBG
