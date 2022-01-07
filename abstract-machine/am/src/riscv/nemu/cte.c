5b8de79163208640377bf72d8fc702821a5876b3
tracer-ics2021 <tracer@njuics.org>
2022-01-07 12:35:04 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 4939437a4d4a4de69744a3e0ffabbb67   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  12:35:04 up  2:25,  1 user,  load average: 0.40, 0.42, 0.44

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -52,7 +52,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   ret->pdir=NULL;
   ret->np=0;//printf("%d\n",((char *)&ret->np-(char *)ret)/4);
 //  extern void* new_page(size_t nr_page);
-  ret->gpr[2]=(uintptr_t)ret;//(uintptr_t)new_page(8)+8*4096;//printf("%08x\n",heap.end);
+  ret->gpr[2]=(uintptr_t)ret-4;//(uintptr_t)new_page(8)+8*4096;//printf("%08x\n",heap.end);
 //  printf("kcontext %08x\n",heap.start);
   return ret;
 }
