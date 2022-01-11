bc49bc2f105a4a8014b8cdd5307d7796416cd4eb
tracer-ics2021 <tracer@njuics.org>
2022-01-12 05:35:41 +0800

    >  compile 201220073  Static hostname: cao-VirtualBox        Icon name: computer-vm          Chassis: vm       Machine ID: 65bd2a6e3c8e4b8690d33d583c23ad4c          Boot ID: 75793e2add4a4fda975a4a20016be337   Virtualization: oracle Operating System: Ubuntu 21.10           Kernel: Linux 5.13.0-19-generic     Architecture: x86-64  Hardware Vendor: innotek GmbH   Hardware Model: VirtualBox  05:35:41 up 8 min,  1 user,  load average: 0.19, 0.43, 0.34

--- nemu/src/isa/riscv32/instr/special.h
+++ nemu/src/isa/riscv32/instr/special.h
@@ -24,10 +24,11 @@ def_EHelper(csrrs) {
   rtl_mv(s,id_dest->preg,now);
   rtl_or(s,now,now,t0);
 }
-
+extern int cyf_M;
 def_EHelper(mret){
   rtl_mv(s,&s->dnpc,&MEPC);
   rtlreg_t temp=(MSTATUS>>7)&1;
+  assert(cyf_M==0);cyf_M=1;
   MSTATUS^=(MSTATUS&MASK)^((1<<7)|(temp<<3));
 }
 
--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -1,9 +1,10 @@
 #include <isa.h>
-
+int cyf_M=0;
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
+  cyf_M=0;
   cpu.sr[0x341]._32=epc;
   cpu.sr[0x342]._32=NO;//Log("ERRERNO:%08x",NO);
   rtlreg_t old=(cpu.sr[0x300]._32>>3)&1;
@@ -12,7 +13,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
 }
 
 word_t isa_query_intr() {//Log("%08x",cpu.sr[0x300]._32);//&&((cpu.sr[0x304]._32>>7)&1)&&((cpu.sr[0x344]._32>>7)&1)
-  if(cpu.INTR&&((cpu.sr[0x300]._32>>3)&1)){
+  if(cpu.INTR&&((cpu.sr[0x300]._32>>3)&1)&&cyf_M){
     cpu.INTR = false;//Log("%08x",IRQ_TIMER);
     return IRQ_TIMER;
   }
